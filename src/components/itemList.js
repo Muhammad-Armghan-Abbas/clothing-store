56cbf3f00763c954d75bd0b9fb48dfc720094f52
Armaghan Abbas <armaghanabbas5@gmail.com>
2024-04-20 16:04:09 +0500

    UPDATE 2

--- src/components/itemList.js
+++ src/components/itemList.js
@@ -2,11 +2,14 @@ import { useContext } from 'react';
 import Item from './item.js'
 import CartContext from './Context/CartContext.js';
 function ItemList() {
-const item=useContext(CartContext);
-const items=item.item;
+const a=useContext(CartContext);
+const items=a.item;
+const filteredProducts = items.filter(item => {
+    return item.category === "men's clothing" || item.category === "women's clothing"
+  })
     return (
         <>{
-            items.map(it => <Item
+            filteredProducts.map(it => <Item
                 type={it.category}
                 name={it.title}
                 price={it.price}
